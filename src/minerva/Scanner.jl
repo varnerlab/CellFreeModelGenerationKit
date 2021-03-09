5a4a2b84d241c72fc0b1c105f95f36292899f6ee
Jeffrey Varner <jdv27@cornell.edu>
2021-03-08 19:18:44 -0500

    Update Scanner.jl
    need to think ..

---
+++ src/minerva/Scanner.jl
@@ -0,0 +1,27 @@
+function scan(records::Dict{Int64,Any}(); logger::Union{Nothing,SimpleLogger})::VLResult
+
+    # initialize -
+    # ...
+
+    try
+
+        # ok: think about me -
+
+    catch error
+        return VLResult(error)
+    end
+end
+
+function scan(record::Array{String,1}; logger::Union{Nothing,SimpleLogger})::VLResult
+
+    # initialize -
+    # ...
+
+    try
+
+        # ok: think about me -
+        
+    catch error
+        return VLResult(error)
+    end
+end
