c07f43f0dd6e0a573259ac6bbcdac57aaf562027
Jeffrey Varner <jdv27@cornell.edu>
2021-03-09 19:03:32 -0500

    Update Parser.jl
    working on grn parser

--- src/minerva/Parser.jl
+++ src/minerva/Parser.jl
@@ -1,9 +1,14 @@
+# -- PRIVATE METHODS ------------------------------------------------------------------------------------------ #
+# ------------------------------------------------------------------------------------------------------------- #
+
+# -- PUBLIC METHODS ------------------------------------------------------------------------------------------- #
 function grn_parse(records::Dict{Int64,Array{MinervaToken,1}}; 
     logger::Union{Nothing,SimpleLogger} = nothing)::VLResult
 
     # initialize -
     try
     
+
     catch error
         return VLResult(error)
     end
@@ -11,3 +16,4 @@ end
 
 function grn_parse(record::Array{MinervaToken,1})::VLResult
 end
+# ------------------------------------------------------------------------------------------------------------- #
